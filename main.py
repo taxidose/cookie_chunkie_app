f66871b310a64462d62f7917b616de27e2d34292
taxidose <patrick.tagsold@outlook.de>
2021-04-01 00:29:29 +0200

    1

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
-class MyGridLayout(GridLayout):
+class MainGrid(Widget):
+    pass
 
-class MyApp(App):
+
+class CookieChunkieApp(App):
     def build(self):
-        return MyGridLayout()
+        return MainGrid()
+
+        #return Button(
+            #text="Hello", pos=(50, 50), size=(100, 100), size_hint=(None, None))
 
 
-
-
+if __name__ == '__main__':
+    CookieChunkieApp().run()
