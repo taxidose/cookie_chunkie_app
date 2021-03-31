eb1e56342c20e311340fab447401f39a8cfad47f
taxidose <patrick.tagsold@outlook.de>
2021-03-31 20:21:49 +0200

    1

---
+++ main.py
@@ -0,0 +1,17 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGridLayout(GridLayout):
+
+class MyApp(App):
+    def build(self):
+        return MyGridLayout()
+
+
+
+
