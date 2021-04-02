11630bf8395df31f7dccf1558c3a345830bc28b2
taxidose <patrick.tagsold@outlook.de>
2021-04-02 20:35:12 +0200

    Home-Screen Design

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
 
-class MainGrid(Widget):
+class MainGrid(GridLayout):
     pass
 
 
